185e054008df302bddc25be34427fd102ec9b20e
Tommy Ku <zoo1929@outlook.com>
2017-01-16 22:23:40 +0800

    ADD: featured section to top of all posts

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -6,6 +6,14 @@ def preview?(item)
   item[:preview] && item[:preview] == true
 end
 
+def special?(item)
+  item[:special] && item[:special] == true
+end
+
+def sorted_special_articles
+  sorted_articles.select { |item| special?(item) && !preview?(item) }
+end
+
 def abstract?(item)
   !item[:abstract].nil?
 end
